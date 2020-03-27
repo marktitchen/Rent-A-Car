3a3833b6e799c1619b146e28e1ae7c46bd5b93d9
Mark Titchen <46524303+marktitchen@users.noreply.github.com>
2020-03-27 17:20:22 +0000

    Manufacturer controller created

---
+++ Controllers/ManufacturerController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Rent_A_Car.Models;
+
+namespace Rent_A_Car.Controllers
+{
+    public class ManufacturerController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Manufacturer
+        public ActionResult Index()
+        {
+            return View(db.Manufacturers.ToList());
+        }
+
+        // GET: Manufacturer/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Manufacturer manufacturer = db.Manufacturers.Find(id);
+            if (manufacturer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(manufacturer);
+        }
+
+        // GET: Manufacturer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Manufacturer/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,ManufacturerName")] Manufacturer manufacturer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Manufacturers.Add(manufacturer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(manufacturer);
+        }
+
+        // GET: Manufacturer/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Manufacturer manufacturer = db.Manufacturers.Find(id);
+            if (manufacturer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(manufacturer);
+        }
+
+        // POST: Manufacturer/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,ManufacturerName")] Manufacturer manufacturer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(manufacturer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(manufacturer);
+        }
+
+        // GET: Manufacturer/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Manufacturer manufacturer = db.Manufacturers.Find(id);
+            if (manufacturer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(manufacturer);
+        }
+
+        // POST: Manufacturer/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Manufacturer manufacturer = db.Manufacturers.Find(id);
+            db.Manufacturers.Remove(manufacturer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
